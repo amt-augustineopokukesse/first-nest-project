ddd4cfe3237beb70eb3be59f32ac609e80dbcc03
Augustine Opoku-Kesse <augustine.opoku-kesse@amalitech.com>
2024-07-10 08:48:43 +0000

    feat: implement nest exception filters

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -1,19 +1,35 @@
-import { Controller, Get, Post, Body } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  HttpException,
+  HttpStatus,
+  UseFilters,
+  ForbiddenException,
+} from '@nestjs/common';
 import { CreateCatDto } from './dto/create-cat.dto';
 import { CatsService } from './cats.service';
-import { Cat } from './interfaces/cat.interface';
+import { HttpExceptionFilter } from 'src/http-exception/http-exception.filter';
+// import { Cat } from './interfaces/cat.interface';
 
 @Controller('cats')
 export class CatsController {
   constructor(private catsService: CatsService) {}
 
   @Post()
+  @UseFilters(new HttpExceptionFilter())
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   async create(@Body() createCatDto: CreateCatDto) {
-    this.catsService.create(createCatDto);
+    // this.catsService.create(createCatDto);
+    throw new ForbiddenException();
   }
 
   @Get()
-  async findAll(): Promise<Cat[]> {
-    return this.catsService.findAll();
+  //   async findAll(): Promise<Cat[]> {
+  //     return this.catsService.findAll();
+  //   }
+  async findAll() {
+    throw new HttpException('Forbidden', HttpStatus.FORBIDDEN);
   }
 }
