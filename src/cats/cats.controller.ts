c798ca73968e590dc8b9cda888c7ae2a547d9768
Augustine Opoku-Kesse <augustine.opoku-kesse@amalitech.com>
2024-07-10 14:58:02 +0000

    feat: implement nest pipes

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -5,12 +5,13 @@ import {
   Body,
   HttpException,
   HttpStatus,
-  UseFilters,
-  ForbiddenException,
+  Param,
+  ParseIntPipe,
+  ValidationPipe,
 } from '@nestjs/common';
 import { CreateCatDto } from './dto/create-cat.dto';
 import { CatsService } from './cats.service';
-import { HttpExceptionFilter } from 'src/http-exception/http-exception.filter';
+// import { HttpExceptionFilter } from 'src/http-exception/http-exception.filter';
 // import { Cat } from './interfaces/cat.interface';
 
 @Controller('cats')
@@ -18,11 +19,8 @@ export class CatsController {
   constructor(private catsService: CatsService) {}
 
   @Post()
-  @UseFilters(new HttpExceptionFilter())
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  async create(@Body() createCatDto: CreateCatDto) {
-    // this.catsService.create(createCatDto);
-    throw new ForbiddenException();
+  async create(@Body(new ValidationPipe()) createCatDto: CreateCatDto) {
+    this.catsService.create(createCatDto);
   }
 
   @Get()
@@ -31,5 +29,10 @@ export class CatsController {
   //   }
   async findAll() {
     throw new HttpException('Forbidden', HttpStatus.FORBIDDEN);
+  }
+
+  @Get(':id')
+  async findOne(@Param('id', ParseIntPipe) id: number) {
+    return this.catsService.findOne(id);
   }
 }
