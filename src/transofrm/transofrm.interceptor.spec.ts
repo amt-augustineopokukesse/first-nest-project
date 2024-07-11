959f6887f68dda091a11b42771d3ce146255054a
Augustine Opoku-Kesse <augustine.opoku-kesse@amalitech.com>
2024-07-11 08:09:18 +0000

    feat: implement nest interceptors

---
+++ src/transofrm/transofrm.interceptor.spec.ts
@@ -0,0 +1,7 @@
+import { TransofrmInterceptor } from './transofrm.interceptor';
+
+describe('TransofrmInterceptor', () => {
+  it('should be defined', () => {
+    expect(new TransofrmInterceptor()).toBeDefined();
+  });
+});
